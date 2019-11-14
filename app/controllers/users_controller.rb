3980f64d8a376dbf0b4df35d5be94f49426e4e6b
graemenickerson <graeme.nickerson@gmail.com>
2019-11-14 00:46:46 +0000

    Adds rest of user authentication logic.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,23 @@
 class UsersController < ApplicationController
-  has_secure_password
 
   def new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to "/", notice: 'Account created successfully'
     else
-      redirect_to '/signup'
+      flash[:error] = 'An error occured!'
+      render 'new'
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password_digest, :password_confirmation)
+    params.require(:user).permit(:f_name, :l_name, :email, :password, :password_confirmation)
   end
 end
