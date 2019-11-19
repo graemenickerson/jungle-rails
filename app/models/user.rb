c5e7ab6c3763261f14c244168d22e908e02ba000
graemenickerson <graeme.nickerson@gmail.com>
2019-11-19 22:04:23 +0000

    Adds tests and refactors user login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ActiveRecord::Base
   validates :f_name, :l_name, :password, :email, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 3 }, confirmation: true
+
+
+  def self.authenticate_with_credentials(input_email, input_password)
+    user = User.where(email: input_email)[0]
+
+    if user && user.authenticate(input_password)
+      return user
+    else
+      return nil
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe User, type: :model do
       subject.password = '1234'
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to eq(["Password confirmation doesn't match Password", "Password confirmation doesn't match Password"])
-
     end
 
     it "is not valid when email is not unique" do 
@@ -27,7 +26,6 @@ RSpec.describe User, type: :model do
       subject.email = nil
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to eq(["Email can't be blank"])
-
     end
 
     it "is not valid without first name" do
@@ -40,7 +38,6 @@ RSpec.describe User, type: :model do
       subject.l_name = nil
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to eq(["L name can't be blank"])
-
     end
 
     it "is not valid when password is shorter than 3 characters" do
@@ -49,6 +46,14 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to eq(["Password is too short (minimum is 3 characters)"])
     end
+
+  end
+
+  describe '.authenticate_with_credentials' do
+    subject { described_class.new(f_name: "Electric", l_name: "Chariot", email: "test@test.com", password: "12345", password_confirmation: "12345")}
+
+    it "is not valid if user does not exist" 
+    it "does not allow login with wrong password"
 
   end
 
